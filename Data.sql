3e526db52098c2794d7ac4f071b7bb7d1e6b6b4c
Deepali2201 <86304130+Deepali2201@users.noreply.github.com>
2023-04-22 04:15:55 +0530

    Add files via upload

---
+++ Data.sql
@@ -0,0 +1,16 @@
+CREATE TABLE customers (
+    customer_id INT,
+    customer_name VARCHAR(255),
+    country VARCHAR(255),
+    age INT
+);
+
+INSERT INTO customers (customer_id, customer_name, country, age)
+VALUES (1, 'John Smith', 'USA', 30),
+       (2, 'Jane Doe', 'Canada', 42),
+       (3, 'Alex Kim', 'USA', 20),
+       (4, 'Emily Chen', 'China', 28),
+       (5, 'Tom Johnson', 'USA', 37),
+       (6, 'Lisa Lee', 'Korea', 24);
+
+SELECT * FROM customers WHERE country = 'USA' AND age > 25;
